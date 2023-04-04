5681bfbed86dde6b79ecd45e035d59432d0bebaa
Matheus <matheus.andrade.lopes@hotmail.com>
2023-04-03 21:20:34 -0300

    Aula do dia 03/04

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
+import './style.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const estilosBotao = {
+    marginTop:12, paddingTop:8, paddingBottom:8, backgroundColor:'blueviolet', color:'white',
+    border:'none', widht:'100%', borderRadius:8
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const textoDoRotulo = 'Nome'
+
+const obterTextoBotao = () => {
+    return 'Enviar'
+}
+
+const App = () => {
+    return <div style={{margin:"auto", width:768, backgroundColor: "#EEE",
+    padding:12, borderRadius:8}}>
+        <label className="rotulo" htmlFor='nome' style={{display: 'block', marginBottom:4}}>
+            {textoDoRotulo}
+        </label>
+        <input type='text' id='nome' style={{paddingTop:8, paddingBottom:8,borderStyle:'hidden', widht:'100%', borderRadius:8,
+            outline:'none',boxSizing:'border-box'}}/>
+            <button style={estilosBotao}>{obterTextoBotao}Enviar</button>
+    </div>
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector("#root")
+)
